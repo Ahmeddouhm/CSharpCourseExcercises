ba19ece7ef6ba0d3464211092bd1552ae5729813
Ahmeddouhm <a.douha0@gmail.com>
2025-12-01 21:41:59 +0200

    Add OOP Task 1 "BankAccount"

---
+++ OOP Excercises/BankAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Excercises
+{
+    internal class BankAccount
+    {
+        public string? AccId { get; set; }
+        public string? CardId { get; set; }
+        public string? Name { get; set; }
+        public decimal Balance { get; set; }
+        public int Transactions { get; set; }
+        public static int OnlineUsers { get; set; }
+
+        public BankAccount(string? accId, string? cardId, string? name, decimal balance)
+        {
+            this.CardId = cardId;
+            this.Name = name;
+            this.Balance = balance;
+            this.Transactions = 0;
+            OnlineUsers++;
+        }
+
+        public void Deposit(decimal amount) 
+        {
+            if(amount == 0)
+                Console.WriteLine("Enter Valid Value");
+            this.Balance += amount;
+            Console.WriteLine($"Successfully Deposited {amount}");
+            Console.WriteLine($"Your Balance Is : {Balance}");
+            Transactions++;
+        }
+        public void Withdraw(decimal amount) 
+        {
+            if(amount > Balance)
+                Console.WriteLine("Enter Valid Value");
+            this.Balance -= amount;
+            Console.WriteLine($"Successfully Withdrawed {amount}");
+            Console.WriteLine($"Your Balance Is : {Balance}");
+            Transactions++;
+        }
+
+        public void ViewBalance() 
+        {
+            Console.WriteLine($"Your Current Balance is : {Balance:C2}, {DateTime.Now} ");
+        }
+    }
+}
--- OOP Excercises/Program.cs
+++ OOP Excercises/Program.cs
@@ -7,7 +7,17 @@ Person p1 = new Person(
     salary: 17000 ,
     children: 2 
     );
+
+BankAccount acc1 = new BankAccount("1" , "54615484984" , "Ahmed Mamdouh" , 10000);
+BankAccount acc2 = new BankAccount("2" , "54615454561" , "Mohamed Mamdouh" , 3240);
+BankAccount acc3 = new BankAccount("3" , "54615454561" , "Hassan Mamdouh" , 9640);
+BankAccount acc5 = new BankAccount("4" , "54615454561" , "Fares Mamdouh" , 8450);
+acc1.ViewBalance();
+acc1.Deposit(325);
+acc2.Withdraw(400);
+Console.WriteLine(BankAccount.OnlineUsers);
+
+
 Console.WriteLine($"{p1}");
-//Console.WriteLine($"Name : {p1.FirstName} {p1.LastName} ,\nAge : {p1.Age} ,\nChildren : {p1.Children} ,\nSalary : {p1.Salary}");
 
 Console.ReadKey();
