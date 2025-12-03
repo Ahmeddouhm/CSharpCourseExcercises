8c5ac119d8b7b01d4bcebdbc9873de67f8fb5beb
Ahmeddouhm <a.douha0@gmail.com>
2025-12-03 02:29:37 +0200

    Update Randomizer

--- Randomizer/Program.cs
+++ Randomizer/Program.cs
@@ -14,10 +14,18 @@ do
     switch (userInput)
     {
         case "1":
-            GenerateNumber(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            Console.Write("Enter Min Number : ");
+            int minNumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter Max Number : ");
+            int maxNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("=====================");
+            GenerateNumber(minNumber , maxNumber);
             break;
         case "2":
-            GenerateString();
+            Console.Write("Enter String Length : ");
+            int stringLength = int.Parse(Console.ReadLine());
+            Console.WriteLine("=====================");
+            GenerateString(stringLength);
             break;
         case "ex":
             isFinished = true;
@@ -31,14 +39,16 @@ static void GenerateNumber(int min , int max)
     Random rnd = new Random();
     int value = rnd.Next(min, max);
     Console.WriteLine($"Number is {value}");
+    Console.WriteLine("=====================");
 }
-static void GenerateString() 
+static void GenerateString(int stLn) 
 {
     Random rnd = new Random();
     var sb = new StringBuilder();
-    while (sb.Length < 5)
+    while (sb.Length < stLn)
     {
         sb.Append((char)(rnd.Next(0, 2) == 0 ? rnd.Next(65, 91) : rnd.Next(97, 123)));
     }
     Console.WriteLine($"String is {sb}");
+    Console.WriteLine("=====================");
 }
