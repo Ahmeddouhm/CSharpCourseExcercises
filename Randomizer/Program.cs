cc6dba8c132ca37e9f64f243efaf5a17ff199cf6
Ahmeddouhm <a.douha0@gmail.com>
2025-12-03 04:33:19 +0200

    some Logic

--- Randomizer/Program.cs
+++ Randomizer/Program.cs
@@ -4,39 +4,39 @@ Console.WriteLine("=====================");
 Console.WriteLine("Welcome To Randomizer");
 Console.WriteLine("=====================");
 bool isFinished = false;
-do
-{
-    Console.Write("Select Random Type " +
-        "\n1) Number" +
-        "\n2) String" +
-        "\n>> ");
-    string userInput = Console.ReadLine();
-    switch (userInput)
-    {
-        case "1":
-            Console.Write("Enter Min Number : ");
-            int minNumber = int.Parse(Console.ReadLine());
-            Console.Write("Enter Max Number : ");
-            int maxNumber = int.Parse(Console.ReadLine());
-            Console.WriteLine("=====================");
-            GenerateNumber(minNumber , maxNumber);
-            break;
-        case "2":
-            Console.Write("Enter String Length : ");
-            int stringLength = int.Parse(Console.ReadLine());
-            Console.WriteLine("=====================");
-            GenerateString(stringLength);
-            break;
-        case "ex":
-            isFinished = true;
-            break;
-        default:
-            Console.WriteLine("=====================");
-            Console.WriteLine("Enter A Valid Command !");
-            Console.WriteLine("=====================");
-            break;
-    }
-} while (!isFinished);
+//do
+//{
+//    Console.Write("Select Random Type " +
+//        "\n1) Number" +
+//        "\n2) String" +
+//        "\n>> ");
+//    string userInput = Console.ReadLine();
+//    switch (userInput)
+//    {
+//        case "1":
+//            Console.Write("Enter Min Number : ");
+//            int minNumber = int.Parse(Console.ReadLine());
+//            Console.Write("Enter Max Number : ");
+//            int maxNumber = int.Parse(Console.ReadLine());
+//            Console.WriteLine("=====================");
+//            GenerateNumber(minNumber , maxNumber);
+//            break;
+//        case "2":
+//            Console.Write("Enter String Length : ");
+//            int stringLength = int.Parse(Console.ReadLine());
+//            Console.WriteLine("=====================");
+//            GenerateString(stringLength);
+//            break;
+//        case "ex":
+//            isFinished = true;
+//            break;
+//        default:
+//            Console.WriteLine("=====================");
+//            Console.WriteLine("Enter A Valid Command !");
+//            Console.WriteLine("=====================");
+//            break;
+//    }
+//} while (!isFinished);
 static void GenerateNumber(int min , int max) 
 {
     Random rnd = new Random();
@@ -55,3 +55,31 @@ static void GenerateString(int stLn)
     Console.WriteLine($"String is {sb}");
     Console.WriteLine("=====================");
 }
+string buffer = "ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/123456789/!@#$%^&*[]";
+string[] newbuffer = buffer.Split('/' , 4);
+foreach (var item in newbuffer)
+{
+    Console.WriteLine(item);
+    Console.WriteLine(newbuffer.Length);
+}
+    Console.WriteLine(newbuffer.Length);
+
+static void GenerateComplexString(int stLn) 
+{
+    Random rnd = new Random();
+    var sb = new StringBuilder();
+    while (sb.Length < stLn)
+    {
+        sb.Append((char)(rnd.Next(0, 2) == 0 ? rnd.Next(65, 91) : rnd.Next(97, 123)));
+    }
+    Console.WriteLine($"String is {sb}");
+    Console.WriteLine("=====================");
+}
+
+
+/*
+ * small? 1
+ * capital? 2
+ * nums? 3
+ * special? 4
+ */ 
