1b49cd77742fdf373cc6f8483cf66156b26d630e
Ahmeddouhm <a.douha0@gmail.com>
2025-12-04 04:50:31 +0200

    Add Exceptions section

---
+++ Exceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿CRUDApp app1 = new CRUDApp();
+app1.ConnectDatabase();
+
+class CRUDApp 
+{
+    string connectionString = null;
+    public void ConnectDatabase() 
+    {
+        if (connectionString==null)
+        {
+            throw new DatabaseNotFoundException();
+        }
+    }
+}
+
+class DatabaseNotFoundException : ApplicationException 
+{
+    public DatabaseNotFoundException()
+    {
+        Console.WriteLine("Database Connection Failed => Database Not Found || Custom Exception");
+    }
+}
