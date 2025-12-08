cad881b91a278e75b35664463454c0c6d6ac64d2
Ahmeddouhm <a.douha0@gmail.com>
2025-12-08 06:00:42 +0200

    Add List Code Section

---
+++ ListsExcercises/Program.cs
@@ -0,0 +1,10 @@
+﻿// Lists Creation
+List<string> projectTeam = new List<string>() { "Ahmed Mamdouh", "Ziad Zakaria", "Youssef Mohamed", "Youssef Gaber", "Youssef Gohary" };
+var students = new List<string>();
+students.Add("Menna");
+students.Add("Mohamed");
+students.Add("Ahmed");
+foreach (var name in students)
+{
+    Console.Write(name+" ");
+}
